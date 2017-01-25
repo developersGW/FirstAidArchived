0686aa79514ef8e68bca32c72627100beb17efb8
SecanoLibre <josua.kiefner@tutanota.de>
2017-01-25 21:06:20 +0100

    Began to add changelog.

--- app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -17,7 +17,7 @@ public final class R {
 		public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f080067;
 	}
 	public static final class id {
-		public static final int item_touch_helper_previous_elevation = 0x7f0c0005;
+		public static final int item_touch_helper_previous_elevation = 0x7f0c0006;
 	}
 	public static final class styleable {
 		public static final int[] RecyclerView = { 0x010100c4, 0x7f01005d, 0x7f01005e, 0x7f01005f, 0x7f010060 };
